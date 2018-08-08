959c4ccc8e34efa3d76120131260203f463364cc
lanpong <hdy95@outlook.com>
2018-08-08 23:00:55 +0800

    find lowest cost node

---
+++ grokking algorithms/find lowest cost node.py
@@ -0,0 +1,11 @@
+# 狄克斯特拉
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
