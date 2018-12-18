ce0abc5474405f7f1be47d08b73993ed6a90c694
lanpong <hdy95@outlook.com>
2018-12-18 10:06:52 +0800

    base converter

---
+++ Learning-Javascript-Data-Structures-and-Algorithms/Stacks/base-converter.js
@@ -0,0 +1,67 @@
+class Stack {
+    constructor(...items){
+      this._items = []
+  
+      if(items.length>0)
+        items.forEach(item => this._items.push(item) )
+  
+    }
+  
+    push(...items){
+      //push item to the stack
+       items.forEach(item => this._items.push(item) )
+       return this._items;
+  
+    }
+  
+    pop(count=0){
+      //pull out the topmost item (last item) from stack
+      if(count===0)
+        return this._items.pop()
+       else
+         return this._items.splice( -count, count )
+    }
+  
+    peek(){
+      // see what's the last item in stack
+      return this._items[this._items.length-1]
+    }
+  
+    size(){
+      //no. of items in stack
+      return this._items.length
+    }
+  
+    isEmpty(){
+      // return whether the stack is empty or not
+      return this._items.length==0
+    }
+  
+    toArray(){
+      return this._items;
+    }
+}
+
+function baseConverter(decNumber, base) {
+
+    var remStack = new Stack(),
+    rem,
+    baseString = '',
+    digits = '0123456789ABCDEF';
+
+    while (decNumber > 0) {
+        rem = Math.floor(decNumber % base);
+        remStack.push(rem);
+        decNumber = Math.floor(decNumber / base);
+    }
+
+    while (!remStack.isEmpty()) {
+        baseString += digits[remStack.pop()];
+    }
+
+    return baseString;
+}
+
+// 简单的进制转化
+console.log(baseConverter(1024, 2));
+console.log(baseConverter(1024, 16));
