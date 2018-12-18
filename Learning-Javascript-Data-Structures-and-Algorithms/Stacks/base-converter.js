20ce86b699159c8c0b8abb0a3fee3fa43c2edc76
lanpong <hdy95@outlook.com>
2018-12-18 20:11:08 +0800

    format style

--- Learning-Javascript-Data-Structures-and-Algorithms/Stacks/base-converter.js
+++ Learning-Javascript-Data-Structures-and-Algorithms/Stacks/base-converter.js
@@ -1,44 +1,46 @@
 class Stack {
-    constructor(...items){
-      this._items = []
+    constructor(...items) {
+        this._items = [];
   
-      if(items.length>0)
+        if (items.length>0) {
+            items.forEach(item => this._items.push(item));
+        }
+  
+    }
+  
+    push(...items) {
+        //push item to the stack
         items.forEach(item => this._items.push(item) )
-  
+        return this._items;
+    }
+
+    pop(count=0) {
+        //pull out the topmost item (last item) from stack
+        if (count===0) {
+            return this._items.pop();
+        }
+        else {
+            return this._items.splice(-count, count);
+        }
     }
   
-    push(...items){
-      //push item to the stack
-       items.forEach(item => this._items.push(item) )
-       return this._items;
-  
+    peek() {
+        // see what's the last item in stack
+        return this._items[this._items.length-1];
     }
   
-    pop(count=0){
-      //pull out the topmost item (last item) from stack
-      if(count===0)
-        return this._items.pop()
-       else
-         return this._items.splice( -count, count )
+    size() {
+        //no. of items in stack
+        return this._items.length;
     }
   
-    peek(){
-      // see what's the last item in stack
-      return this._items[this._items.length-1]
+    isEmpty() {
+        // return whether the stack is empty or not
+        return this._items.length==0;
     }
   
-    size(){
-      //no. of items in stack
-      return this._items.length
-    }
-  
-    isEmpty(){
-      // return whether the stack is empty or not
-      return this._items.length==0
-    }
-  
-    toArray(){
-      return this._items;
+    toArray() {
+        return this._items;
     }
 }
 
