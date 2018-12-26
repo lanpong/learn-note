9bcf4922c02648b24726c4b86a50ecdfa8f73948
lanpong <hdy95@outlook.com>
2018-12-26 09:43:18 +0800

    stack

--- Learning-Javascript-Data-Structures-and-Algorithms/Stacks/new-Stacks.js
+++ Learning-Javascript-Data-Structures-and-Algorithms/Stacks/new-Stacks.js
@@ -2,7 +2,7 @@ class Stack {
     constructor(...items) {
         this._items = [];
 
-        if (items.length>0) {
+        if (items.length > 0) {
             items.forEach(item => this._items.push(item));
         }
 
@@ -16,7 +16,7 @@ class Stack {
 
     pop(count=0) {
         //pull out the topmost item (last item) from stack
-        if (count===0) {
+        if (count === 0) {
             return this._items.pop();
         } else {
             return this._items.splice(-count, count);
@@ -35,10 +35,52 @@ class Stack {
 
     isEmpty() {
         // return whether the stack is empty or not
-        return this._items.length==0;
+        return this._items.length == 0;
     }
 
     toArray() {
         return this._items;
     }
+}
+
+/**
+ * 以上是构造式写法
+ * -------------------
+ */
+function Stack() {
+    let items = [];
+
+    // push elements to the stack
+    this.push = function(element) {
+        items.push(element);
+    };
+
+    // pop elements to the stack
+    this.pop = function() {
+        return pop.items();
+    };
+
+    // peek the element from the top stack
+    this.peek = function() {
+        return items[items.length-1];
+    };
+
+    // empty
+    this.isEmpty = function() {
+        return items.length == 0;
+    };
+
+    // clear stack
+    this.clear = function() {
+        items = [];
+    };
+
+    // print stack
+    this.print = function() {
+        console.log(items.toString());
+    };
+
+    //
+
+
 }
