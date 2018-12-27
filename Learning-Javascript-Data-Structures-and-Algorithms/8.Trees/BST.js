0216f3c199c7a749ce83d8168465eecf28bee3f7
lanpong <hdy95@outlook.com>
2018-12-27 10:01:20 +0800

    trees

--- Learning-Javascript-Data-Structures-and-Algorithms/8.Trees/BST.js
+++ Learning-Javascript-Data-Structures-and-Algorithms/8.Trees/BST.js
@@ -35,5 +35,135 @@ function BinarySearchTree() {
         }
     };
 
-    
+    // inorder traversal
+    this.inOrderTraverse = function(callback) {
+        inOrderTraverseNode(root, callback);
+    };
+
+    var inOrderTraverseNode = function(node, callback) {
+        if (node !== null) {
+            inOrderTraverseNode(node.left, callback);
+            callback(node.key);
+            inOrderTraverseNode(node.right, callback);
+        }
+    };
+
+    // preorder traversal
+    this.preOrderTraverse = function(callback) {
+        preOrderTraverseNode(root, callback);
+    };
+
+    var preOrderTraverseNode = function(node, callback) {
+        if (node !== null) {
+            callback(node.key);
+            preOrderTraverseNode(node.left, callback);
+            preOrderTraverseNode(node.right, callback);
+        }
+    };
+
+    // post order traversal
+    this.postOrderTraverse = function(callback) {
+        postOrderTraverseNode(root, callback);
+    };
+
+    var postOrderTraverseNode = function(node, callback) {
+        if (node !== null) {
+            postOrderTraverseNode(node.left, callback);
+            postOrderTraverseNode(node.right, callback);
+            callback(node.key);
+        }
+    };
+
+    // find the minimum key of the tree
+    this.min = function() {
+        return minNode(root);
+    };
+
+    var minNode = function(node) {
+        if (node) {
+            while (node && node.left !== null) {
+                node = node.left;
+            }
+
+            return node.key;
+        }
+        return null;
+    };
+
+    //find the max key of the tree
+    this.max = function() {
+        return maxNode(root);
+    };
+
+    var maxNode = function(node) {
+        if (node) {
+            while (node && node.right !== null) {
+                node = node.right;
+            }
+
+            return node.key;
+        }
+        return null;
+    };
+
+    // search for speacific value
+    this.search = function(key) {
+        return searchNode(root, key);
+    };
+
+    var searchNode = function(node, key) {
+        if (node === null) {
+            return false;
+        }
+        if (key < node.key) {
+            return searchNode(node.left, key);
+        } else if (key > node.key) {
+            return searchNode(node.right, key);
+        } else {
+            return true;
+        }
+    };
+
+    // remove a node
+    this.remove = function(key) {
+        root = removeNode(root, key)
+    }
+
+    var removeNode = function(node, key) {
+        if (node === null) {
+            return null;
+        }
+        if (key < node.key) {
+            node.left = removeNode(node.left, key);
+            return node;
+        } else if (key > node.key) {
+            node.right = removeNode(node.right, key);
+            return node;
+        } else {
+            if (node.left === null && node.right === null) {
+                node = null;
+                return node;
+            }
+
+            if (node.left === null) {
+                node = node.right;
+                return node;
+            } else if (node.right === null) {
+                node = node.left;
+                return node;
+            }
+
+            var aux = findMinNode(node.right);
+            node.key = aux.key;
+            node.right = removeNode(node.right, aux.key);
+            return node;
+        }
+    };
+
+    var findMinNode = function(node) {
+        while (node && node.left !== null) {
+            node = node.left;
+        }
+        return node;
+    };
 }
