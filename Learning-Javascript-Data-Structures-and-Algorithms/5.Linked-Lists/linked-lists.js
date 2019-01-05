0a92291adb76fe914fc91ae6091373c7aac0abb3
lanpong <hdy95@outlook.com>
2019-01-05 19:28:56 +0800

    Linked List

--- Learning-Javascript-Data-Structures-and-Algorithms/5.Linked-Lists/linked-lists.js
+++ Learning-Javascript-Data-Structures-and-Algorithms/5.Linked-Lists/linked-lists.js
@@ -1,13 +1,16 @@
 function LinkedList() {
 
+    // 创建链表，链表需要两个属性，一个值，一个指向下一个节点的指针
     let Node = function(element) {
         this.element = element;
         this.next = null;
     };
 
+    // 初始化，即设置长度为0，和设置头节点为null
     let length = 0;
     let head = null;
 
+    // 向链表尾部添加元素
     this.append = function(element) {
         let node = new Node(element),
         current;
@@ -26,9 +29,11 @@ function LinkedList() {
 
         length++;
     };
+
+    // 在特定位置插入元素
     this.insert = function(position, element) {
 
-        if (position >= 0 && position <=length) {
+        if (position >= 0 && position <= length) {
 
             let node = new Node(element),
             current = head,
@@ -55,6 +60,8 @@ function LinkedList() {
             return false;
         }
     };
+
+    // 移除某个位置的元素
     this.removeAt = function(position) {
 
         if (position > -1 && position < length) {
@@ -80,10 +87,13 @@ function LinkedList() {
             return null;
         }
     };
+    
     this.remove = function(element) {
         let index = this.indexOf(element);
         return this.removeAt(index);
     };
+
+    // 返回给定元素的位置，如果没有则返回 -1
     this.indexOf = function(element) {
 
         let current = head,
@@ -99,12 +109,16 @@ function LinkedList() {
 
         return -1;
     };
+
     this.isEmpty = function() {
         return length === 0;
     };
+
     this.size = function() {
         return length;
     };
+
+    // 将LinkedList 转化成一个字符串
     this.toString = function() {
 
         let current = head,
@@ -116,7 +130,9 @@ function LinkedList() {
         }
         return string;
     };
+
     this.print = function() {};
+
     this.getHead = function() {
         return head;
     }
