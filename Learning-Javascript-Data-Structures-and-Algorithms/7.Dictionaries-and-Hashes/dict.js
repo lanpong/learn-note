75207509915e34cec384ca9c241761e8c926d603
lanpong <hdy95@outlook.com>
2019-01-06 14:15:30 +0800

    dict

--- Learning-Javascript-Data-Structures-and-Algorithms/7.Dictionaries-and-Hashes/dict.js
+++ Learning-Javascript-Data-Structures-and-Algorithms/7.Dictionaries-and-Hashes/dict.js
@@ -1,14 +1,21 @@
+/**
+ * 字典：[键:值]
+ * 可以说是关系的映射
+ */
 function Dictionary() {
     var items = {};
 
+    // 是否存在某个键值
     this.has = function(key) {
         return key in items;
     };
 
+    // 添加新元素
     this.set = function(key, value) {
         items[key] = value;
     };
 
+    // 通过使用键值来从字典中移除键值对应的数据值
     this.delete = function(key) {
         if (this.has(key)) {
             delete items[key];
@@ -17,10 +24,12 @@ function Dictionary() {
         return false;
     };
 
+    // 通过键值查找特定的数值并返回
     this.get = function(key) {
         return this.has(key) ? items[key] : undefined;
     };
 
+    // 将字典所包含的所有数值以数组的形式返回
     this.values = function() {
         var values = [];
         for (var k in items) {
@@ -31,6 +40,7 @@ function Dictionary() {
         return values;
     };
 
+    // 将字典所包含的所有键名以数组的形式返回
     this.keys = function() {
         return Object.keys(items);
     };
@@ -39,12 +49,13 @@ function Dictionary() {
         return items;
     };
 
+    // 返回字典所包含元素的数量
     this.size = function() {
         return Object.keys[items].length;
     };
 
+    // 删除所有元素
     this.clear = function() {
         items = {}
     };
-
 }
