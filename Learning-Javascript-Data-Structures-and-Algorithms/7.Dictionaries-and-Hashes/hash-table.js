fa51387b3d309c452a4417be7740d02964cdebf1
lanpong <hdy95@outlook.com>
2019-01-06 17:31:55 +0800

    hash table

--- Learning-Javascript-Data-Structures-and-Algorithms/7.Dictionaries-and-Hashes/hash-table.js
+++ Learning-Javascript-Data-Structures-and-Algorithms/7.Dictionaries-and-Hashes/hash-table.js
@@ -8,4 +8,36 @@ function HashTable() {
         }
         return hash%37;
     };
+
+    // 实现 put 方法
+    this.put = function(key, value) {
+        var position = loseloseHashCode(key);
+        console.log(position + ' - ' + key);
+        table[position] = value;
+    };
+
+    // 实现 get 方法
+    this.get = function(key) {
+        return table[loseloseHashCode(key)];
+    };
+
+    // remove
+    this.remove = function(key) {
+        table[loseloseHashCode(key)] = undefined;
+    };
+
+    // print
+    this.print = function() {
+        for (var i=0; i<table.length; i++) {
+            if (table[i] !== undefined) {
+                console.log(i + ': ' + table[i]);
+            }
+        }
+    };
 }
+
+// test case
+var hash = new HashTable();
+hash.put('Gandalf', 'gandalf@email.com');    // 19 - Gandalf
+hash.put('John', 'john@email.com');          // 29 - John
+hash.put('Tyrion', 'tyrion@email.com');      // 16 - Tyrion
